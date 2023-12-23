c9af49e8baa1c00f8efeefa86baca2798d141f89
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-23 16:57:37 -0600

    Refactor start screen UI

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,29 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Start Screen'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(255, 230, 203, 244),
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {}, 
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+          child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
