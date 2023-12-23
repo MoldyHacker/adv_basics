ba7db5796d16328125f290a0c39c06173ef77e41
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-23 16:41:32 -0600

    Refactor main.dart: Add gradient background and import StartScreen
    My attempt was pretty close, but it wasn't broken out into files. so now im coding along.

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset('assets/images/quiz-logo.png', width: 200),
-              const SizedBox(
-                height: 20,
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.deepPurple,
+                  Colors.purple,
+                ],
               ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Start Quiz',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+            child: const StartScreen(),
+            ),
       ),
     ),
   );
