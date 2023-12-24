3fdfb0311d0f5069db6b0d65dc5ff76554eef5e6
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-23 23:17:36 -0600

    Add getSummaryData() method to ResultsScreen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      final question = questions[i];
+      final answer = chosenAnswers[i];
+
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(context) {
